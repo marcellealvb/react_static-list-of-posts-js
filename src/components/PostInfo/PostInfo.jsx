32f8defee3d98d8eb402ecb2b9deff2b3ace1ab5
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-31 19:18:19 -0300

    developing list of posts

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,20 @@
-export const PostInfo = () => <>Put the post here</>;
+import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
+import '../UserInfo/UserInfo.scss';
+import '../CommentList/CommentList.scss';
+import './PostInfo.scss';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo" key={post.id}>
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">qui est esse</h3>
+      <p>
+        {' Posted by  '}
+        <UserInfo user={post.user} />
+      </p>
+    </div>
+    <p className="PostInfo__body">{post.body}</p>
+    <hr />
+    <CommentList comments={post.comments} />
+  </div>
+);
